6e288b5982bb705a16d1a81f9ee6042f1fdac371
rahelf <frick.rahel@gmail.com>
2017-07-26 09:59:00 +0200

    update

--- AllSequences.py
+++ AllSequences.py
@@ -1,7 +1,13 @@
-import os, os.path
+#
+####################################
+# Copyright Rahel Frick
+# email: frick.rahel@gmail.com
+####################################
+
+import os, os.path, re
 from Sequence import Sequence
 import matplotlib.pyplot as plt
-from mutant_scFv_analyzer_constants import *
+from constants import *
 import seaborn as sns; sns.set()
 import numpy as np
 import pandas as pd
@@ -10,16 +16,22 @@ import pandas as pd
 class AllSequences(object):
 	'''Handles one folder as downloaded from GATC containing .seq, .fas and .abc files'''
 	
-	def __init__(self, directory, reference_file):
+	def __init__(self, directory, reference_file, start_site, linker, end_site):
 
-		ref_seq = Sequence(reference_file)
+		ref_seq = Sequence(reference_file, start_site, linker, end_site)
+
+		# getting start and end indices of linker in reference sequence
+		find_linker = re.search(linker, ref_seq.sequence)
+		self.start_linker = find_linker.start()
+		self.end_linker = find_linker.end()
 
 		self.directory = directory
 		self.file_list = []
 		for filename in os.listdir(self.directory):
+			
 			# print filename
 			if filename.endswith('.fas'):
-				self.file_list.append(os.path.abspath(self.directory)+'/'+filename)
+				self.file_list.append(os.path.join(os.path.abspath(self.directory), filename))
 		self.names = []
 		self.mutationlist =[]
 		self.aa_sequences= []
@@ -38,18 +50,17 @@ class AllSequences(object):
 		self.aa_mutation_numbers = []
 		self.stop_codons = []
 
+
 		for seq in self.file_list:
 			#self.no_seq_files += 1
-			test_sequence = Sequence(seq, reference=ref_seq)
+			test_sequence = Sequence(seq, start_site, linker, end_site, reference=ref_seq)
 			if test_sequence.bad_sequence == False:
 
 				self.names.append(test_sequence.name)
 				self.stop_codons.append(test_sequence.stop_codons)
-				#print test_sequence.stop_codons
 
 				self.mutationlist.append(test_sequence.mutationlist)
 				self.aa_mutationlist.append(test_sequence.aa_mutationlist)
-
 				self.mutation_numbers.append(test_sequence.mutations)
 				
 				if test_sequence.truncation == False:
@@ -87,9 +98,9 @@ class AllSequences(object):
 			if 'ochre' in item:
 				ochres += 1
 
-		self.amber_ratio = float(ambers)/number_of_seq
-		self.opal_ratio = float(opals)/number_of_seq
-		self.ochre_ratio = float(ochres)/number_of_seq
+		#self.amber_ratio = float(ambers)/number_of_seq
+		#self.opal_ratio = float(opals)/number_of_seq
+		#self.ochre_ratio = float(ochres)/number_of_seq
 
 		#print self.amber_ratio, self.opal_ratio, self.ochre_ratio
 
@@ -101,6 +112,10 @@ class AllSequences(object):
 		self.mutation_frequency_aa = np.mean(np.array(self.aa_functional_sequences))/float(len(ref_seq.aa_sequence))
 
 
+		self.dna_sequence_length = len(ref_seq.sequence)
+		self.aa_sequence_length = len(ref_seq.aa_sequence)
+		#print self.dna_sequence_length
+		#print self.aa_sequence_length
 
 		for mutation in self.mutation_types:
 			if len(self.mutation_types_count.keys()) < 25:
@@ -114,18 +129,21 @@ class AllSequences(object):
 
 		self.mean_mutation_number = np.mean(self.mutation_numbers)
 		self.stdev_mutation_number = np.std(self.mutation_numbers)
-		print self.names
-		print self.stop_codons
+		#print self.names
+		#print self.stop_codons
+		print '#########################\nWriting output'
 		self.write_output()
+		print 'plotting mutation distribution over sequence'
 		self.plot_mutation_distribution()
+		print 'Plotting the distribution of numbers of mutations over all sequences'
 		self.plot_mutation_numbers_distribution()
-
+		print 'Plotting mutation types'
 		self.plot_mutation_types()
 		
 
 
 	def write_output(self):
-		outfile = self.directory + '/sequence_report.txt'
+		outfile = os.path.join(self.directory, 'sequence_report.txt')
 		f = open(outfile, 'w')
 		f.write('Please check the following sequences manually:\n')
 		for item in self.bad_sequences:
@@ -139,9 +157,9 @@ class AllSequences(object):
 		f.write(' There are on average %f mutations per sequence. Standard deviation is %f' %(self.mean_mutation_number, self.stdev_mutation_number))
 		f.write('\nMutation frequency on DNA level: %s %%\nMutation frequncy on AA level: %s %%' %(self.mutation_frequency_dna*100, self.mutation_frequency_aa*100))
 		f.write('\n\nPercentage of truncated sequences based on different stop codons:\n')
-		f.write('Amber stops (%s) are contained in %f %% of the sequences' %(stop_codons['amber'], self.amber_ratio*100))
-		f.write('\nOpal stops (%s) are contained in %f %% of the sequences' %(stop_codons['opal'], self.opal_ratio*100))
-		f.write('\nOchre stops (%s) are contained in %f %% of the sequences' %(stop_codons['ochre'], self.ochre_ratio*100))
+		#f.write('Amber stops (%s) are contained in %f %% of the sequences' %(stop_codons['amber'], self.amber_ratio*100))
+		#f.write('\nOpal stops (%s) are contained in %f %% of the sequences' %(stop_codons['opal'], self.opal_ratio*100))
+		#f.write('\nOchre stops (%s) are contained in %f %% of the sequences' %(stop_codons['ochre'], self.ochre_ratio*100))
 
 		f.write('\n\nDetailed information about individual sequences:\n------------------------------------------------------------------\n')
 		for i in range(len(self.names)):
@@ -161,34 +179,39 @@ class AllSequences(object):
 		try:
 			bin_max = max(self.mutation_positions)
 		except ValueError:
+			print "WARNING: No mutations recognized. I cannot plot the distributions of mutations."
 			bin_max = 1
+
 		number_of_bins = np.linspace(-0.5, bin_max +0.5, bin_max+2)
-		y, x, _ = plt.hist(self.mutation_positions, bins = number_of_bins, color='#444B6E', linewidth=0)
-
-		plt.axvline(min(linker_positions), linewidth=2, color='crimson')
-		plt.axvline(max(linker_positions), linewidth=2, color='crimson')
-		plt.title('Mutation positions on DNA level')
-		plt.ylim(0, y.max()+1)
-		plt.xlim(-0.5, max(self.mutation_positions)+1)
-		plt.ylabel('Absolute frequency')
-		plt.xlabel('Position in Sequence')
-		plt.savefig(self.directory+'/mutation_distribution.png', dpi=300)
-
-		plt.clf()
-		try:
-			bin_max = max(self.aa_mutation_positions)
-		except ValueError:
-			bin_max = 1
-		number_of_bins = np.linspace(-0.5, bin_max + 0.5, bin_max + 2)
-		y, x, _ = plt.hist(self.aa_mutation_positions, bins=number_of_bins, color='#444B6E', linewidth=0)
-		plt.axvline(min(linker_positions)/3, linewidth=2, color='crimson')
-		plt.axvline(max(linker_positions)/3, linewidth=2, color='crimson')
-		plt.title('Mutation positions on amino acid level')
-		plt.ylim(0, y.max()+1)
-		plt.xlim(-0.5, max(self.aa_mutation_positions)+1)
-		plt.ylabel('Absolute frequency')
-		plt.xlabel('Position in aa Sequence')
-		plt.savefig(self.directory+'/aa_mutation_distribution.png', dpi=300)		
+		if len(self.mutation_positions) == 0:
+			plt.plot([])
+		else:
+			y, x, _ = plt.hist(self.mutation_positions, bins = number_of_bins, color='#444B6E', linewidth=0)
+			plt.axvline(self.start_linker, linewidth=2, color='crimson')
+			plt.axvline(self.end_linker, linewidth=2, color='crimson')
+			plt.yticks(np.arange(0, max(y)+1, 10))
+			plt.title('Mutation positions on DNA level')
+			plt.ylim(0, y.max()+1)
+			plt.xlim(-0.5, self.dna_sequence_length+1)
+			plt.ylabel('Absolute frequency')
+			plt.xlabel('Position in Sequence')
+			plt.savefig(os.path.join(self.directory, 'mutation_distribution.png'), dpi=300)
+			plt.clf()
+			try:
+				bin_max = max(self.aa_mutation_positions)
+			except ValueError:
+				bin_max = 1
+			number_of_bins = np.linspace(-0.5, bin_max + 0.5, bin_max + 2)
+			y, x, _ = plt.hist(self.aa_mutation_positions, bins=number_of_bins, color='#444B6E', linewidth=0)
+			plt.axvline(self.start_linker/3, linewidth=2, color='crimson')
+			plt.axvline(self.end_linker/3, linewidth=2, color='crimson')
+			plt.title('Mutation positions on amino acid level')
+			plt.yticks(np.arange(0, self.aa_sequence_length+1, 10))
+			plt.ylim(0, y.max()+1)
+			plt.xlim(-0.5, self.aa_sequence_length+1)
+			plt.ylabel('Absolute frequency')
+			plt.xlabel('Position in aa Sequence')
+			plt.savefig(os.path.join(self.directory, 'aa_mutation_distribution.png'), dpi=300)		
 
 
 	def plot_mutation_numbers_distribution(self):
@@ -196,17 +219,19 @@ class AllSequences(object):
 		try:
 			bin_max = max(self.mutation_numbers)
 		except ValueError:
+			print 'WARNING: No mutations recognized. I cannot print the distribution of mutation rates.'
 			bin_max = 1
 		mutation_num_bins = np.linspace(-0.5, bin_max + 0.5, bin_max+2)
-		#print mutation_num_bins
 		y, x, _ = plt.hist(self.mutation_numbers, bins=mutation_num_bins, histtype='stepfilled', color='#444B6E')
 		ax = plt.gca()
 		plt.ylim(0, y.max()+1)
+		plt.yticks(np.arange(1, max(y)+1, 1.0))
+		plt.xticks(np.arange(0, max(x) +1, 10))
 		plt.xlim(-0.5, max(self.mutation_numbers)+1)
 		plt.title('Mutations per sequence on DNA level')
 		plt.ylabel('Absolute frequency')
 		plt.xlabel('Number of mutations')
-		plt.savefig(self.directory+'/mutation_number_distributions.png', dpi=300)
+		plt.savefig(os.path.join(self.directory, 'mutation_number_distributions.png'), dpi=300)
 
 		plt.clf()
 		try:
@@ -217,15 +242,20 @@ class AllSequences(object):
 		y, x, _ = plt.hist(self.aa_functional_sequences, bins=mutation_num_bins, histtype='stepfilled', color='#444B6E')
 		ax = plt.gca()
 		plt.ylim(0, y.max()+1)
+		plt.yticks(np.arange(1, max(y)+1, 1.0))
+		plt.xticks(np.arange(0, max(x) +1, 10))
 		plt.xlim(-0.5, max(self.aa_functional_sequences)+1)
 		plt.title('Mutations per sequence on amino acid level')
 		plt.ylabel('Absolute frequency')
 		plt.xlabel('Number of mutations')
-		plt.savefig(self.directory+'/aa_mutation_number_distributions.png', dpi=300)
+		plt.savefig(os.path.join(self.directory, 'aa_mutation_number_distributions.png'), dpi=300)
 
 
 	def plot_mutation_types(self):
 		plt.clf()
+		if len(list(self.mutation_types_count.values())) == 0:
+			print 'WARNING: No mutations recognized. I cannot plot the mutation types.'
+			return None
 		ser = pd.Series(list(self.mutation_types_count.values()), index=pd.MultiIndex.from_tuples(self.mutation_types_count.keys()))
 		df =ser.unstack().fillna(0)
 		df.shape
@@ -235,7 +265,7 @@ class AllSequences(object):
 		plt.ylabel('Mutate from ...')
 		plt.xlabel('Mutate to ...')
 		ax.plot()
-		plt.savefig(self.directory+'/mutation_types_distribution.png', dpi=300)
+		plt.savefig(os.path.join(self.directory, 'mutation_types_distribution.png'), dpi=300)
 
 		plt.clf()
 		ser = pd.Series(list(self.aa_mutation_types_count.values()), index=pd.MultiIndex.from_tuples(self.aa_mutation_types_count.keys()))
@@ -247,7 +277,7 @@ class AllSequences(object):
 		plt.ylabel('Mutate from ...')
 		plt.xlabel('Mutate to ...')
 		ax.plot()
-		plt.savefig(self.directory+'/aa_mutation_types_distribution.png', dpi=300)
+		plt.savefig(os.path.join(self.directory, 'aa_mutation_types_distribution.png'), dpi=300)
 
 
 
--- Sequence.py
+++ Sequence.py
@@ -1,106 +1,146 @@
-from mutant_scFv_analyzer_constants import *
+#from AnMut import *
+#
+####################################
+# Copyright Rahel Frick
+# email: frick.rahel@gmail.com
+####################################
+
 import re
+from constants import *
 import subprocess
 from Bio.Seq import Seq
+import os.path
+from helper import align_sequences
 
 
 class Sequence(object):
 	'''Handles one single fasta file as obtained from GATC'''
 
-	def __init__(self, filename, reference=''):
+	def __init__(self, filename, start_site, linker, end_site, reference=''):
 
+		print '--------------------------------------'
 		self.filename = filename
-		self.name = filename.split('/')[-1].rstrip('.fas')
+		self.name = os.path.split(filename)[-1].rstrip('.fas')
 		f = open(filename)
-		
+
 		seq_dat = f.readlines()
 		self.raw_sequence = ''
 
-		# DNA level
-		self.identifier = seq_dat[0].lstrip('>')[:8].rstrip('\n')
+		# extract identifier and raw sequence from the fas file (DNA level)
+		self.identifier = seq_dat[0].lstrip('>').rstrip('\n')
 		try:
 			self.raw_sequence = seq_dat[1]
 		except:
 			pass
+		#print 'Sequence %s has a length of %i bp.' %(self.identifier, len(self.raw_sequence))
+
+
+		# Initiate empty lists for mutation positions, mutation types and fully specified mutations (DNA level)
 		self.bad_sequence = False
 		self.mutation_positions= []
 		self.mutation_types = []
 		self.mutationlist = []
-
-		# aa level
+		
+		# Make empty sequence object for aa sequece
 		self.aa_sequence = Seq('')
+
+		# Initiate same empty lists to keep track of mutations but this time on aa level.
 		self.aa_mutation_positions= []
 		self.aa_mutation_types = []
 		self.aa_mutationlist = []
-		self.truncation = True
 
-		# Find start codon in sequnce (including RE sites) and translate
-		self.read_sequence()
+		# By default I assume a sequence is not truncated
+		self.truncation = False
+		#self.truncation = True
+
+		# Find start and end sites in sequence. Makes a clean self.sequence including the RE sites. 
+		self.read_sequence(start_site, linker, end_site)
 		self.stop_codons = []
 
+		#print 'Identifier: %s\nSequence: %s' %(self.identifier, self.raw_sequence)
+
+
+		# In the beginning I also assume that sequences are not from an NNK library with length polymorphism
+		self.NNK_status = False
+
+		#print self.bad_sequence
 		if self.bad_sequence == False:
 
-			# Compare to reference sequence
+			# Compare to reference sequence if there is a reference sequence. Now we want the same start position as in the reference sequence and translate from here.
 			if reference != '':
 				self.start_pos = reference.start_pos
 				self.translate_sequence()
+				# We also want to align the test sequence to the reference sequence
 				self.align_to_reference(reference)
+
+			# If this is the reference sequence itself (aka there is no other given reference sequence) we want to determine the start position at a start codon. And then translate the sequence from here.
 			elif reference == '':
 				self.start_pos = self.dna_seq_object.find(start_codon)
 				self.translate_sequence()
-				print self.sequence[383:446]
 
-
+			#make a list of all codons in a sequence to later check for stop codons.
 			self.codons = [self.sequence[self.start_pos:][i:i+3] for i in range(0,len(self.sequence[self.start_pos:]),3)]
 			#print self.codons
+
+			# now checking for stop codons and making a list of the ones found in the sequence.
 			for key in stop_codons.keys():
 				if stop_codons[key] in self.codons:
 					self.stop_codons.append(key)
 
 
 
-	def read_sequence(self):
+	def read_sequence(self, start_site, linker, end_site):
+		'''Takes a start site, linker site, and end_site and returns a cleaned self.sequence and a Seq object of it (self.dna_seq_object).
+		'''
+
+		# Try to find the sequence between the start and end site. upper or lower case letters are accepted in the start and end sites.
 		try:
-			sequence = re.findall('(?<=%s)[A-Z, a-z]{0,}(?=%s)' %(RE_site_1, RE_site_2), self.raw_sequence, re.IGNORECASE)[0]
-			self.sequence= RE_site_1 + sequence + RE_site_2
+			sequence = re.findall('(?<=%s)[A-Z, a-z]{0,}(?=%s)' %(start_site, end_site), self.raw_sequence, re.IGNORECASE)[0]
+			self.sequence= start_site + sequence + end_site
 			self.dna_seq_object = Seq(self.sequence)
 
+		# If a sequence cannot be identified based on the given start and end sites, it is a bad sequence.
 		except:
 			self.bad_sequence = True
 
+
 	def translate_sequence(self):
+
+		print 'Translating sequence %s...' %self.filename
+		#the productive sequence starts at the start position. This is a start codon in the reference but could be something else in the mutants.
 		self.productive_sequence = Seq(self.sequence[self.start_pos:])
+
+		# using Biopython to translate the productive part of the sequence
 		self.aa_sequence = self.productive_sequence.translate()
 
 		
 	def align_to_reference(self, reference):
-		self.mutations = 0
-		if not len(self.sequence) == len(reference.sequence):
+
+		print 'Aligning sequence %s...' %self.filename
+		
+		# Does the test sequence have the same length as the reference sequence?
+		diff = len(self.sequence) - len(reference.sequence)
+
+		# these are sequences that seem to have a frameshift mutation or truncation or by more than the allowed length limit (internal RE site or sequencing problem)
+		if not diff % 3 == 0 or abs(diff) > allowed_length_limit:
 			self.bad_sequence = True
+			print 'Out of frame mutation or truncation or incomplete sequence likely'
+		
+		# these are sequences with same length as reference
+		elif len(self.sequence) == len(reference.sequence):
+			print 'expected length'
+	
+			self.mutation_positions, self.mutation_types, self.mutation_list, self.mutations = align_sequences(self.sequence, reference.sequence)
+
+		# sequences that have in frame mutations (possibly NNK mutants)
 		else:
-			for i in range(len(reference.sequence)):
-				if reference.sequence[i] == self.sequence[i] or reference.sequence[i] == self.sequence[i].upper() or self.sequence[i] in 'Nn':
-					pass
-				else:
-					self.mutations += 1
-					self.mutation_positions.append(i+1)
-					self.mutation_types.append((reference.sequence[i], self.sequence[i].upper()))
+			self.NNK_status = True
+			print 'Sequence could  be an NNK mutant with length polymorphism'# %self.filename
+			self.bad_sequence = True
 
-					mutation = reference.sequence[i] + str(i+1) + self.sequence[i].upper()
-					# print mutation
-					self.mutationlist.append(mutation)
+		#self.truncation = False	
 
-		self.truncation = False	
-		self.aa_mutations = 0
 		if len(self.aa_sequence) != len(reference.aa_sequence):
 			self.truncation = True
 		else:
-			for i in range(len(reference.aa_sequence)):
-				if reference.aa_sequence[i] == self.aa_sequence[i] or self.aa_sequence[i] == 'X':
-					pass
-				else:
-					self.aa_mutations +=1
-					self.aa_mutation_positions.append(i+1)
-					aa_mutation = reference.aa_sequence[i] +str(i+1) +self.sequence[i]
-					self.aa_mutationlist.append(aa_mutation)
-					self.aa_mutation_types.append((reference.aa_sequence[i], self.aa_sequence[i]))
+			self.aa_mutation_positions, self.aa_mutation_types, self.aa_mutationlist, self.aa_mutations = align_sequences(self.aa_sequence, reference.aa_sequence)
